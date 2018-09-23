1b75c2307464704b9168dff6b1db335389e93e48
Philip Lanphar <pjlanphar@gmail.com>
2018-09-22 18:18:58 -0700

    Minor fix.

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  sideNavbar: boolean = false;
+  sideNavbar: boolean;
 
   constructor() {
   }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core'
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from '../app.routes';
 
 
