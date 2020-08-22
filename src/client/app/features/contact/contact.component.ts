882d366e4b8f383f14a3e21e6c47b9e2019cca0e
Philip Lanphar <philip.lanphar@gmail.com>
2020-08-22 13:55:00 +0200

    Updated form to display telephone number and added more keywords to index.html file.

--- src/client/app/features/contact/contact.component.ts
+++ src/client/app/features/contact/contact.component.ts
@@ -26,7 +26,7 @@ export class ContactComponent implements OnInit {
       'firstName': new FormControl('', Validators.required),
       'lastName': new FormControl(''),
       'email': new FormControl('', Validators.required),
-      'subject': new FormControl(''),
+      'telephone': new FormControl('', [Validators.pattern('[0-9]{0-10}')]),
       'theme': new FormControl(''),
       'date': new FormControl(new Date()),
       'message': new FormControl(''),
